9fbf42b49fc6355a57fcf06ff196286734c2238b
leoseoane1 <34175092+leoseoane1@users.noreply.github.com>
2020-03-30 09:06:43 -0500

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,102 @@
+var mysql = require('mysql');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var session=require('express-session');
+var helmet=require('helmet')
+var app2=express();
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "Leo",
+  password: "NikolaJokic1!",
+  database: "seschema"
+});
+
+app2.use(bodyParser.urlencoded({extended : true}));
+app2.use(express.static(path.join(__dirname, '/')));
+app2.use(bodyParser.json());
+app2.use(bodyParser.raw());
+app2.use(bodyParser.text());
+app2.use(helmet());
+
+app2.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app2.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/website.html'));
+});
+
+app2.get('/login.html',function(req,res){
+  res.sendFile(path.join(__dirname+'/login.html'));
+});
+
+app2.get('/register.html',function(req,res){
+  res.sendFile(path.join(__dirname+'/register.html'));
+});
+
+app2.get('/homepage.html',function(req,res){
+  res.sendFile(path.join(__dirname+'/homepage.html'));
+});
+
+app2.listen(process.env.port || 3000);
+console.log('Running at Port 3000');
+
+app2.post('/register.html',function(req,res){
+    con.connect(function(err) {
+    if (err) throw err;
+    var sql="Insert into dbuser (Username,Pswd,Email) values ?";
+    var values=[[req.body.username,req.body.password,req.body.username]];
+      con.query(sql,[values], function (err, result) {
+      if (err) throw err;
+      req.session.loggedin = true;
+      req.session.username = username;
+      });
+    });
+    res.write("Thank you for registering");
+     return res.redirect('/homepage.html');
+});
+
+app2.post('/login.html', function(req, res) {
+  con.connect(function(err){
+    if (err) throw err;
+    var values =[[req.body.username,req.body.password]]
+    var sql= 'SELECT * FROM dbuser WHERE username = ? AND password = ?';
+    if (values) {
+      con.query(sql,[values], function(error, result) {
+        if (values.length > 0) {
+          req.session.loggedin = true;
+          req.session.username=values[0];
+          return res.redirect('/homepage.html');
+        } else {
+          res.send('Incorrect Username and/or Password!');
+        }
+        res.end();
+      });
+    } else {
+      res.send('Please enter Username and Password!');
+      res.end();
+    }
+  });
+});
+
+// app2.post('/homepage.html', function(req,res){
+//   con.connect(function(err){
+//     if (err) throw err;
+//     var values =[[]]
+//     var sql='select distinct recipe_name
+//             from contain join recipe on Recipe_ID=RcpID
+//             where recipe_name NOT IN(Select distinct recipe_name
+//             from (contain join recipe on RcpID=Recipe_ID)
+//             where ing_nm NOT IN (select distinct Ingr
+//             from  (owns join dbuser on usrn=username)
+//             where username =req.body.username))';
+//     con.query(sql,[values],function(error,result){
+//
+//             })
+//
+//   })
+// })
